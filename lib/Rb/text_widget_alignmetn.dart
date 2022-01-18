aa2a7e18acc24e2e6b60849d72d29b208204587f
rb <707mauryaji@gmail.com>
2022-01-18 15:54:16 +0530

    text widget_alignment

---
+++ lib/Rb/text_widget_alignmetn.dart
@@ -0,0 +1,50 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to rbinfotech.com',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'rb infotech pvt',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.yellow,
+            ),
+          ),
+        ),
+        body: const Center(
+          child: Text(
+            "rb  infotech private limited first program",
+            style: TextStyle(
+              fontSize: 40.0,
+            ),
+            textAlign: TextAlign.center,
+            //textAlign: TextAlign.left,
+            //textDirection: TextDirection.ltr,
+            //textDirection: TextDirection.rtl,
+            //softwrap take boolean function
+            //if true then it take space according him
+            //softWrap: false,
+            softWrap: true,
+          ),
+        ),
+      ),
+    );
+  }
+}
