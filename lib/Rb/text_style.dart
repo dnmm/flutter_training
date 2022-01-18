d93e0157d700437910306315f274b2e568ab83b1
rb <707mauryaji@gmail.com>
2022-01-18 15:53:39 +0530

    text style

---
+++ lib/Rb/text_style.dart
@@ -0,0 +1,52 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to rbinfotech.com',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'rb infotech pvt',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.yellow,
+            ),
+          ),
+        ),
+        body: const Center(
+          child: Text(
+            "rb  infotech private limited \n first program",
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 20.0,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+              //backgroundColor: Colors.yellow,
+              //letterSpacing: 10.0,
+              //wordSpacing: 20.0,
+              //decoration: TextDecoration.underline,
+              decorationColor: Colors.black,
+              fontFamily: "Times New Roman",
+              //decorationStyle: TextDecorationStyle.dashed,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
