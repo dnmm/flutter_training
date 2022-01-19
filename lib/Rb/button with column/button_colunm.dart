1b435c07fff4e1d6def1421f65bb31cefc9a97be
rb <707mauryaji@gmail.com>
2022-01-20 00:35:21 +0530

    button with column

---
+++ lib/Rb/button with column/button_colunm.dart
@@ -0,0 +1,89 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Textbutt());
+}
+
+class Textbutt extends StatelessWidget {
+  const Textbutt({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to rbinfotech.com',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'rb infotech pvt',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.yellow,
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 140, vertical: 3),
+                child: Text(
+                  "this is a text widget",
+                  style: TextStyle(
+                    fontSize: 22.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 140, vertical: 3.2),
+                child: MaterialButton(
+                  splashColor: Colors.cyan,
+                  padding: const EdgeInsets.all(10.0),
+                  onPressed: () {},
+                  child: const Text(
+                    "TEXT BUTTON 1",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 140, vertical: 3.4),
+                child: Text(
+                  "this is a text widget 2",
+                  style: TextStyle(
+                    fontSize: 22.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 140, vertical: 3.6),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "TEXT BUTTON 2",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
