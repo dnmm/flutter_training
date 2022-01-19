1152c1fb48e93cfaef29344cf7c360593b88ae90
rb <707mauryaji@gmail.com>
2022-01-20 00:36:50 +0530

    full screen background color

---
+++ lib/Rb/full color screen/fullscreen_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "rb demo app",
+      home: Container(
+        color: Colors.red,
+      ),
+    ),
+  );
+}
