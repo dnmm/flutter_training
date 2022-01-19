5cbd5c06267d29873dd9a09ab5dfc94a518a8ab5
rb <707mauryaji@gmail.com>
2022-01-20 00:37:34 +0530

    button with row

---
+++ lib/Rb/row button/row_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RObutton());
+}
+
+class RObutton extends StatelessWidget {
+  const RObutton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "sample app",
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "Sample app",
+            ),
+          ),
+          body: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                MaterialButton(
+                  splashColor: Colors.green,
+                  onPressed: () {},
+                  child: const Text(
+                    "material button",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+                MaterialButton(
+                  splashColor: Colors.green,
+                  onPressed: () {},
+                  child: const Text(
+                    "material button",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
