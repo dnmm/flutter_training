4cbf5d3125414dfa0a66365f6b30d8bdb0898549
rb <707mauryaji@gmail.com>
2022-01-20 00:37:14 +0530

    splash screen color

---
+++ lib/Rb/full color screen/splash_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      title: "rb",
+      home: const Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              /* CircularProgressIndicator(
+                backgroundColor: Colors.white,
+              )*/
+              Text(
+                "splansh\nscreen",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w800,
+                  fontFamily: "Cursive",
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
