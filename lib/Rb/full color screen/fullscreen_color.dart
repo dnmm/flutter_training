ceadd7b8dd584878b59267b9767b0d56c1db24c8
rb <707mauryaji@gmail.com>
2022-01-24 22:14:38 +0530

    24/1/2022

--- lib/Rb/full color screen/fullscreen_color.dart
+++ lib/Rb/full color screen/fullscreen_color.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      title: "rb demo app",
+      title: "demo app",
       home: Container(
         color: Colors.red,
       ),
