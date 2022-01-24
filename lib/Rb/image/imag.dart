c66ff85be19d6247d625db95022d411a199b1b63
rb <707mauryaji@gmail.com>
2022-01-24 22:09:10 +0530

    image

---
+++ lib/Rb/image/imag.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class Imagepage extends StatelessWidget {
+  const Imagepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter testing page',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'flutter testing page',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.yellow,
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.all(0.0),
+                child: Text(
+                  "my first image",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(0.0),
+                child: Image.asset(
+                  'assets/images/a.png',
+                  //fit: BoxFit.cover,
+                  height: 200,
+                  width: 200,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(0.0),
+                child: Image.asset(
+                  'assets/images/a.png',
+                  //fit: BoxFit.cover,
+                  height: 200,
+                  width: 200,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
