b217e5e16d3217c317c64c52ef43b8b71b44fa36
rahulmaurya01 <rahul9336446070@gmail.com>
2022-01-28 13:19:01 +0530

    Button

---
+++ lib/Rahul/button/button_1.dart
@@ -0,0 +1,70 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class Firsttextbutton extends StatelessWidget {
+  const Firsttextbutton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'flutter testing page',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.yellow,
+            ),
+          ),
+        ),
+        body: Column(
+          //mainaxisaligment mai varticaly move hota hai
+          //mainAxisAlignment: MainAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.start,
+          //mainAxisAlignment: MainAxisAlignment.end,
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+          //cross axialignment mai horizontical move hota hai
+          //crossAxisAlignment: CrossAxisAlignment.start,
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.end,
+
+          //strach matlab column span hoga pura screen mai center mai jaiyaiga
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
+
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.all(2.0),
+              child: Text(
+                "this is a text widget",
+                style: TextStyle(
+                  fontSize: 22.0,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: MaterialButton(
+                onPressed: () {},
+                child: const Text(
+                  "click",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
