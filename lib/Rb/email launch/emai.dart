37ec6f00bb7fde5d86e8fd9875ad83355c0ca489
rb <707mauryaji@gmail.com>
2022-01-31 11:48:12 +0530

    url_launcher

---
+++ lib/Rb/email launch/emai.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Emaildetaiss extends StatefulWidget {
+  const Emaildetaiss({Key? key}) : super(key: key);
+
+  @override
+  _EmaildetaissState createState() => _EmaildetaissState();
+}
+
+class _EmaildetaissState extends State<Emaildetaiss> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "url email",
+      debugShowCheckedModeBanner: false,
+      home: Emailhome(),
+    );
+  }
+}
+
+Future<void> call(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw "cannot alunch $url";
+  }
+}
+
+launchurl() async {
+  const url = 'https://www.youtube.com/';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw "cannot launch $url";
+  }
+}
+
+launchgoogle() async {
+  const url = 'https://www.google.com/';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw "cannot launch $url";
+  }
+}
+
+launchEmail() async {
+  launch(
+    'mailto:707mauryaji@gmail.com?subject=TestEmail&body=Subscribe webfun please',
+  );
+}
+
+class Emailhome extends StatefulWidget {
+  const Emailhome({Key? key}) : super(key: key);
+
+  @override
+  _EmailhomeState createState() => _EmailhomeState();
+}
+
+class _EmailhomeState extends State<Emailhome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo[900],
+        title: const Text(
+          'url launcher',
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            MaterialButton(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              onPressed: () {
+                call("tel:7651972973");
+              },
+              color: Colors.indigo[900],
+              child: const Text(
+                "call",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            const MaterialButton(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              onPressed: launchurl,
+              color: Colors.indigo,
+              child: Text(
+                'youtube',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            const MaterialButton(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              onPressed: launchEmail,
+              color: Colors.indigo,
+              child: Text(
+                'Email',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            const MaterialButton(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              onPressed: launchgoogle,
+              color: Colors.indigo,
+              child: Text(
+                'google',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Rb/email launch/emailphonewithscroll.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Emaillaunc extends StatelessWidget {
+  const Emaillaunc({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: dead_code
+    return const MaterialApp(title: 'Flutter Demo', home: Emailurlcaunc());
+  }
+}
+
+class Emailurlcaunc extends StatelessWidget {
+  const Emailurlcaunc({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Launchers"),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Card(
+                    color: Colors.white70,
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(color: Colors.white, width: 2.0),
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Padding(
+                            padding: const EdgeInsets.all(30.0),
+                            child: ElevatedButton.icon(
+                              icon: const Icon(Icons.add_to_home_screen),
+                              label: const Text(
+                                "Open Website",
+                                style: TextStyle(fontSize: 25),
+                              ),
+                              onPressed: () => launch("http://google.com"),
+                            )),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(color: Colors.white, width: 2.0),
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(30.0),
+                          child: ElevatedButton.icon(
+                            icon: const Icon(Icons.call),
+                            label: const Text(
+                              "Make a Call",
+                              style: TextStyle(fontSize: 25),
+                            ),
+                            onPressed: () => launch("tel://123"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  Card(
+                    color: Colors.white70,
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(color: Colors.white, width: 2.0),
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(30.0),
+                          child: ElevatedButton.icon(
+                            icon: const Icon(Icons.email),
+                            label: const Text("Send a Email",
+                                style: TextStyle(fontSize: 25)),
+                            onPressed: () => launch(
+                                "mailto:707mauryaji@gmail.com?subject=TestEmail&body=Subscribe webfun please"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(color: Colors.white, width: 2.0),
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(30.0),
+                          child: ElevatedButton.icon(
+                            icon: const Icon(Icons.sms),
+                            label: const Text("facebook",
+                                style: TextStyle(fontSize: 25)),
+                            onPressed: () =>
+                                launch("https://www.facebook.com/friends"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(color: Colors.white, width: 2.0),
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(30.0),
+                          child: ElevatedButton.icon(
+                            icon: const Icon(Icons.sms),
+                            label: const Text("youtube",
+                                style: TextStyle(fontSize: 25)),
+                            onPressed: () => launch("https://www.youtube.com/"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+}
