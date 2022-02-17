a215186d203406fadfa3e96009618aee69eefefd
rahulmaurya01 <rahul9336446070@gmail.com>
2022-02-17 15:54:00 +0530

    listview

---
+++ lib/Rahul/listview/listview.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class Listview extends StatelessWidget {
+  const Listview({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter App Learning',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: Simplelistviewpage());
+  }
+}
+
+class Simplelistviewpage extends StatefulWidget {
+  Simplelistviewpage({Key? key}) : super(key: key);
+  @override
+  _SimplelistviewpageState createState() => _SimplelistviewpageState();
+}
+
+class _SimplelistviewpageState extends State<Simplelistviewpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Listview page"),
+        ),
+        body: ListView(
+          children: [
+            Card(
+                child: ListTile(
+              title: Text("python course"),
+              subtitle: Text('django and tkinter'),
+              leading: Icon(Icons.book),
+              trailing: MaterialButton(
+                color: Colors.blue,
+                onPressed: () {},
+                child: Text('click'),
+              ),
+              //trailing: Icon(Icons.star),
+              //onLongPress: () {},
+              onTap: () {},
+              tileColor: Colors.white,
+            )),
+            Card(
+              child: ListTile(
+                title: Text("flutter course"),
+              ),
+            ),
+            Card(
+                child: ListTile(
+              title: Text("C curse"),
+            )),
+            Card(
+                child: ListTile(
+              title: Text("php curse"),
+            )),
+            Card(
+                child: ListTile(
+              title: Text("python course"),
+              subtitle: Text('django and tkinter'),
+              leading: Icon(Icons.book),
+              trailing: Icon(Icons.star),
+              //onLongPress: () {},
+              onTap: () {},
+              tileColor: Colors.blue[200],
+            )),
+            Card(
+                child: ListTile(
+              title: Text("C curse"),
+            )),
+            Card(
+              child: ListTile(
+                title: Text("flutter course"),
+              ),
+            ),
+            Card(
+                child: ListTile(
+              title: Text("C curse"),
+            )),
+            Card(
+                child: ListTile(
+              title: Text("php curse"),
+            )),
+            Card(
+                child: ListTile(
+              title: Text("python course"),
+              subtitle: Text('django and tkinter'),
+              leading: Icon(Icons.book),
+              trailing: Icon(Icons.star),
+              //onLongPress: () {},
+              onTap: () {},
+              tileColor: Colors.blue[200],
+            )),
+            Card(
+                child: ListTile(
+              title: Text("C curse"),
+            )),
+            Card(
+                child: ListTile(
+              title: Text("php curse"),
+            )),
+          ],
+          // shrinkWrap: true,
+        ));
+  }
+}
---
+++ lib/Rahul/listview/listview1.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class Listviews1 extends StatelessWidget {
+  const Listviews1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter App Learning',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: Listviewpage());
+  }
+}
+
+class Listviewpage extends StatefulWidget {
+  Listviewpage({Key? key}) : super(key: key);
+  @override
+  _ListviewpageState createState() => _ListviewpageState();
+}
+
+class _ListviewpageState extends State<Listviewpage> {
+  List<String> images = [
+    "assets/images/a.png",
+    "assets/images/logo.png",
+    "assets/images/a.png",
+    "assets/images/logo.png",
+    "assets/images/a.png",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter ListView"),
+        ),
+        body: ListView.builder(
+          itemBuilder: (BuildContext, index) {
+            return Card(
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(images[index]),
+                ),
+                title: Text("company name $index"),
+                subtitle: Text("branch"),
+              ),
+            );
+          },
+          itemCount: images.length,
+          //shrinkWrap: false,
+          padding: EdgeInsets.all(5),
+          scrollDirection: Axis.vertical,
+        ));
+  }
+}
