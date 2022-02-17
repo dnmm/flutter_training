306931f040436b54fb2dd1f5290e3e4cdad3b7ad
rahulmaurya01 <rahul9336446070@gmail.com>
2022-02-17 16:09:17 +0530

    image

---
+++ lib/Rahul/image/image.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class Imagepages extends StatelessWidget {
+  const Imagepages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter testing page',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'flutter testing page',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.yellow,
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.all(0.0),
+                child: Text(
+                  "My first image",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(0.0),
+                child: Image.asset(
+                  'assets/images/logo.png',
+                  //fit: BoxFit.cover,
+                  //height: 600,
+                  width: 300,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              // Padding(
+              //   padding: const EdgeInsets.all(0.0),
+              //   child: Image.asset(
+              //     'assets/images/a.png',
+              //     //fit: BoxFit.cover,
+              //     height: 200,
+              //     width: 200,
+              //   ),
+              // )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
