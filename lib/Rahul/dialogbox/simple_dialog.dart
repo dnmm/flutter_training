312546c498a1e718f9970b4d799fc4c6fe178e07
rahulmaurya01 <rahul9336446070@gmail.com>
2022-02-19 14:32:18 +0530

    dialog box

---
+++ lib/Rahul/dialogbox/simple_dialog.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class Simplealert extends StatelessWidget {
+  const Simplealert({Key? key}) : super(key: key);
+
+  static const String _title = 'Dialogbox';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(title: const Text(_title)),
+        drawerEnableOpenDragGesture: false,
+        body: const Center(
+          child: MySimpleDiaglog(),
+        ),
+      ),
+    );
+  }
+}
+
+class MySimpleDiaglog extends StatelessWidget {
+  const MySimpleDiaglog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text(
+            'AlertDialog',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.red,
+            ),
+          ),
+          content: const Text(
+            'do you want to submit ?',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.blue,
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'cancel'),
+              child: const Text('cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+      child: const Text('submit'),
+    );
+  }
+}
