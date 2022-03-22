7acc6639e55be72559837691dec520592dee0fbc
vishalsinghvns <101693250+vishalsinghvns@users.noreply.github.com>
2022-03-22 16:46:58 +0530

    button_navigation2

---
+++ lib/vishal/Button/btn_navigation2.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class Btnnavipage2 extends StatelessWidget {
+  const Btnnavipage2({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: Bottomnavigationbarpage2(),
+    );
+  }
+}
+
+class Bottomnavigationbarpage2 extends StatefulWidget {
+  const Bottomnavigationbarpage2({Key? key}) : super(key: key);
+
+  @override
+  State<Bottomnavigationbarpage2> createState() =>
+      _Bottomnavigationbarpage2State();
+}
+
+class _Bottomnavigationbarpage2State extends State<Bottomnavigationbarpage2> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Settings',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bottom_NavigationBar_ Sample'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.computer),
+            label: 'desktop',
+            backgroundColor: Color.fromARGB(255, 158, 172, 81),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Colors.pink,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
