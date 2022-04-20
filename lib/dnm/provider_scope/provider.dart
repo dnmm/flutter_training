22c248821cd1762d43b8586cb3d3aef5c6f1176e
Dina Nath Maurya <dnm.dpm@gmail.com>
2022-04-20 11:08:34 +0530

    new code

--- lib/dnm/provider_scope/provider.dart
+++ lib/dnm/provider_scope/provider.dart
@@ -1,10 +1,11 @@
 // dependencies: flutter_riverpod: ^1.0.0
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/dnm/extra_learning/riverpod/random_number.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ProviderScope extends StatelessWidget {
-  ProviderScope({Key? key}) : super(key: key);
+  ProviderScope({Key? key, RandomNumberGenerator child}) : super(key: key);
 
   final valueProvider = Provider<int>((ref) {
     return 36;
