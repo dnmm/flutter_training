00f928f1923a875afb72379bb51c965d4abfb948
Dina Nath Maurya <dnm.dpm@gmail.com>
2022-04-20 10:51:55 +0530

    delete

--- lib/dnm/extra_learning/riverpod/example/other/models/todo.dart
+++
@@ -1,9 +0,0 @@
-import 'package:uuid/uuid.dart';
-
-var _uuid = const Uuid();
-
-class Todo {
-  final String id;
-  final String description;
-  Todo({required this.description, required String id}) : id = _uuid.v4();
-}
--- lib/dnm/extra_learning/riverpod/models/todo.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:uuid/uuid.dart';
-
-// var _uuid = uuid();
-
-// class Todo {
-//   final String id;
-//   final String description;
-//   Todo({this.description, String id}) : id = id ?? _uuid.v4;
-// }
