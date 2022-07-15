9e7b6ad7db6b15e55f4242b8ce5956701b201902
babita93 <babitasmaurya@gmail.com>
2022-07-15 15:54:04 +0530

    Update hello.dart

--- lib/babita/hello_world/hello.dart
+++ lib/babita/hello_world/hello.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Cont extends StatelessWidget {
-  
+  const Cont({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
