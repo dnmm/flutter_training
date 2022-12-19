7dfcd6f79e6a572fd04a560ff38f326e02e1befe
Dina Nath Maurya <dnm.dpm@gmail.com>
2022-12-19 10:29:34 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'dnm/1teacher_homework/riverpod_all_step.dart';
 
 void main() {
-  // note - please comment below line and uncomment your run app without ProviderScope
+//## note - please comment below line and uncomment your run app without ProviderScope
   runApp(ProviderScope(child: RiverPodStateManagment()));
+
+//## note -without river pod using uncomment it
 
   // runApp(NamePositionalParameter());
 }
