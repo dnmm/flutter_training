7a3a2d0b1db8e334f3225962f31104831864d379
Dina Nath Maurya <dnm.dpm@gmail.com>
2022-12-19 10:28:01 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,8 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'dnm/1teacher_homework/riverpod_all_step.dart';
 
 void main() {
-  // note - please comment below line and write your run app name
+  // note - please comment below line and uncomment your run app without ProviderScope
   runApp(ProviderScope(child: RiverPodStateManagment()));
 
   // runApp(NamePositionalParameter());
-}
-
-class MainMyApp extends StatelessWidget {
-  const MainMyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
-  }
 }
