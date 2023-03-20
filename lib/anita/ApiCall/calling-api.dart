f3716d70792363732add9ba050cfa3c6b94c3727
tryeno <tryeno.work@gmail.com>
2023-03-20 13:05:07 +0530

    make api call

---
+++ lib/anita/ApiCall/calling-api.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class CallingApi extends StatefulWidget {
+  const CallingApi({Key? key}) : super(key: key);
+
+  @override
+  State<CallingApi> createState() => _CallingApiState();
+}
+
+class _CallingApiState extends State<CallingApi> {
+  final url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    var res =
+        await http.get(Uri.http("jsonplaceholder.typicode.com", "photos"));
+
+    print(res.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/anita/ApiCall/calling-api.dart';
 
-import 'anita/single_child_scrollview.dart';
+void main() => runApp(MaterialApp(title: 'Hello', home: CallingApi()));
 
-void main() => runApp(const MySingleChildScrollView());
 
-class MyApp extends StatelessWidget {
-  const MyApp();
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('AlertDialog Sample')),
-        body: const Center(
-          child: DialogExample(),
-        ),
-      ),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   const MyApp();
 
-class DialogExample extends StatelessWidget {
-  const DialogExample();
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: const Text('AlertDialog Sample')),
+//         body: const Center(
+//           child: DialogExample(),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () => showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          title: const Text('AlertDialog Title'),
-          content: const Text('AlertDialog description'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'Cancel'),
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'OK'),
-              child: const Text('OK'),
-            ),
-          ],
-        ),
-      ),
-      child: const Text('Show Dialog'),
-    );
-  }
-}
+// class DialogExample extends StatelessWidget {
+//   const DialogExample();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return TextButton(
+//       onPressed: () => showDialog<String>(
+//         context: context,
+//         builder: (BuildContext context) => AlertDialog(
+//           title: const Text('AlertDialog Title'),
+//           content: const Text('AlertDialog description'),
+//           actions: <Widget>[
+//             TextButton(
+//               onPressed: () => Navigator.pop(context, 'Cancel'),
+//               child: const Text('Cancel'),
+//             ),
+//             TextButton(
+//               onPressed: () => Navigator.pop(context, 'OK'),
+//               child: const Text('OK'),
+//             ),
+//           ],
+//         ),
+//       ),
+//       child: const Text('Show Dialog'),
+//     );
+//   }
+// }
