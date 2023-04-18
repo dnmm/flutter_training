7a79879655bd4b09a308e769c2d7d79488755977
Dina Nath Maurya <dnm.dpm@gmail.com>
2023-04-18 15:44:20 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/dnm/Button/elevated_button.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'Sharmila/state_provider.dart';
+import 'package:flutter_application_1/dnm/2023/class2/example.dart';
 
 void main() {
 //## note -without river pod using uncomment it
-  runApp(MyElevatedButton());
+  runApp(MyNewApp());
 
-//## note - please comment below line and uncomment your run app without ProviderScope
-  // runApp(ProviderScope(child: RiverPodStateManagment()));
-  runApp(
-    ProviderScope(
-      child: MyWidget(),
-    ),
-  );
+// //## note - please comment below line and uncomment your run app without ProviderScope
+//   // runApp(ProviderScope(child: RiverPodStateManagment()));
+//   runApp(
+//     ProviderScope(
+//       child: MyWidget(),
+//     ),
+//   );
 }
