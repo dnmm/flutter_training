c1d6b6c56b4e568d4bf903c25414dbeaa6b3d492
Dina Nath Maurya <dnm.dpm@gmail.com>
2023-05-15 10:57:27 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '2023_new_training/class1/put_multiple_number_on_screen.dart';
+import 'dnm/container/multiple_container_using_refactore.dart';
 
 void main() {
-//## note - please comment below line and uncomment your run app without ProviderScope
-  // runApp(ProviderScope(child: RiverPodStateManagement()));
-  runApp(
-    ProviderScope(
-      child: MyNewApp(),
-    ),
-  );
-//## note -without river pod using uncomment it
+// //## note - please comment below line and uncomment your run app without ProviderScope
+//   // runApp(ProviderScope(child: RiverPodStateManagement()));
+//   runApp(
+//     ProviderScope(
+//       child: MyNewApp(),
+//     ),
+//   );
+// //## note -without river pod using uncomment it
 
-  // runApp(NamePositionalParameter());
+  runApp(MultipleContoner());
 }
