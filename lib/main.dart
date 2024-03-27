f77504cc9cd3912f8e3635b66f6faa8f014f9845
Anita <rgeet3918@gmail.com>
2024-03-27 12:48:03 +0530

    My tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/anita/alert_dialog.dart';
 import 'package:flutter_application_1/anita/navigation.dart/first_page.dart';
 import 'package:flutter_application_1/anita/navigation.dart/home_page.dart';
+import 'package:flutter_application_1/anita/navigation.dart/second_page.dart';
+import 'package:flutter_application_1/anita/tab_bar.dart';
+import 'package:flutter_application_1/gitanjali/tab_bar.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
@@ -13,6 +16,6 @@ void main() {
 //     ),
 //   );
 // //## note -without river pod using uncomment it
-  runApp(ProviderScope(child: MyHomeNavigation()));
+  runApp(ProviderScope(child: MyTabBar1()));
 //  runApp(MultipleContoner());
 }
