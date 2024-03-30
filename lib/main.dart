24aae128a37de7d0c35036dfce1a66630baaca2a
Anita <rgeet3918@gmail.com>
2024-03-30 13:11:05 +0530

    card widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/anita/alert_dialog.dart';
-import 'package:flutter_application_1/anita/navigation.dart/first_page.dart';
-import 'package:flutter_application_1/anita/navigation.dart/home_page.dart';
-import 'package:flutter_application_1/anita/navigation.dart/second_page.dart';
-import 'package:flutter_application_1/anita/tab_bar.dart';
-import 'package:flutter_application_1/gitanjali/tab_bar.dart';
+import 'package:flutter_application_1/gitanjali/card_widgets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
@@ -16,6 +11,6 @@ void main() {
 //     ),
 //   );
 // //## note -without river pod using uncomment it
-  runApp(ProviderScope(child: MyTabBar1()));
+  runApp(ProviderScope(child: MyCard()));
 //  runApp(MultipleContoner());
 }
