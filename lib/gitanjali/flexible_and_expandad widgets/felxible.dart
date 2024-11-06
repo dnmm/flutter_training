19dda87dd124e4ed72714f3e63184d3152138ee4
Gitanjali Rai <rgeet3918@gmail.com>
2024-11-06 13:03:58 +0530

    flexible and expanded widgets

---
+++ lib/gitanjali/flexible_and_expandad widgets/felxible.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FlexibleWidgets());
+}
+
+class FlexibleWidgets extends StatelessWidget {
+  //@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flexible Example'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              height: 100,
+              color: Colors.red,
+              child: Center(child: Text('Fixed Height Container')),
+            ),
+            Flexible(
+              flex: 2,
+              child: Container(
+                color: Colors.blue,
+                child: Center(child: Text('Flexible Container (Flex: 2)')),
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                color: Colors.green,
+                child: Center(child: Text('Flexible Container (Flex: 1)')),
+              ),
+            ),
+            Container(
+              height: 100,
+              color: Colors.yellow,
+              child: Center(child: Text('Fixed Height Container')),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
